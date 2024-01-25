4ee8477d22d8d2d78c7808e373560dc96a7f64bd
lpritchard57 <157082085+lpritchard57@users.noreply.github.com>
2024-01-24 18:50:37 -0600

    Add files via upload

---
+++ Knock, knock.py
@@ -0,0 +1,8 @@
+print("Knock, knock.")
+print("Who's there?")
+print("Wooden shoe.")
+print("Wooden shoe who?")
+print("Wooden shoe like to hear another joke?")
+print("")
+print("Knock,knock.\nWho's there?\nWooden shoe.\nWooden shoe who?\nWooden shoe like to hear another joke?")
+# The purpose of this program is to utlizie less space in your code and avoid the repetitiveness of the print command.
