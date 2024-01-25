ed445879db6e211ef96a554d18af1bd0258abbe5
lpritchard57 <157082085+lpritchard57@users.noreply.github.com>
2024-01-24 20:34:37 -0600

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,21 @@
+# This is a Python program to demonstrate different data types
+
+# Literal
+x = 78
+print("Literal:", x)
+
+# Integer
+y = -13
+print("Integer:", y)
+      
+# Float
+z = 6.2
+print("Float:", z)      
+
+# String
+Name = "Bob"
+print("String:", Name)
+
+# Boolean
+is_active = False
+print("Boolean:", is_active)
