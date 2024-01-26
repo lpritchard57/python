dab444dbfb00038b6b30232bab1b3df23048630c
lpritchard57 <157082085+lpritchard57@users.noreply.github.com>
2024-01-26 16:52:59 -0600

    Add files via upload

---
+++ mathexample.py
@@ -0,0 +1,13 @@
+# Literal
+length = 8
+
+# Literal
+width = 10
+
+# Literal
+area = 80
+
+
+print("The area of the rectangle with a length of "+str(length)+"\nand a width of "+str(width)+" is "+str(area)+".")
+
+# The purpose of this program is to be able to include numerical values into print statements.
