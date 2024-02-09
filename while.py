cca94cab65c612d0f951be6b3ff9bf0bb1500aa4
lpritchard57 <157082085+lpritchard57@users.noreply.github.com>
2024-02-09 12:36:23 -0600

    Add files via upload

---
+++ while.py
@@ -0,0 +1,18 @@
+count = 99
+
+while count != 2:
+    print (count,"bottles of beer on the wall")
+    print (count,"bottles of beer")
+    print("Take one down, pass it around")
+    print(count-1,"bottles of beer on the wall!\n")
+    count -= 1
+
+print(count,"bottles of beer on the wall")
+print(count,"bottles of beer")
+print("Take one down, pass it around")
+print(count-1,"bottle of beer on the wall!\n")
+
+print(count-1,"bottle of beer on the wall")
+print(count-1,"bottle of beer")
+print("Take it down, pass it around")
+print("No bottles of beer on the wall!")
