df9ff960798c1fb29d7c9b2280529d027ab32fd5
lpritchard57 <157082085+lpritchard57@users.noreply.github.com>
2024-03-01 17:49:03 -0600

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,7 @@
+
+programming_classes = ('Intro to Python', 'Advanced Python', 'Database Essentials',
+                       'Web Development Basics', 'Data Structures in Python',
+                       'Web Design Fundamentals')
+for item in programming_classes:
+    print(item)
+print("The tuple has", len(programming_classes), "items.")
