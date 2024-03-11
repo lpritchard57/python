c973be08be7da1b20ac62f266480e994d17344dc
lpritchard57 <157082085+lpritchard57@users.noreply.github.com>
2024-03-10 19:51:22 -0500

    Add files via upload

---
+++ modules/main.py
@@ -0,0 +1,16 @@
+from math_operations import calculator, geometry
+
+result = calculator.add(5,3)
+print("Addition result:", result)
+
+result = calculator.subtract(10,4)
+print("Subtraction result:", result)
+
+result = geometry.rectangle(4,7)
+print("Area of rectangle:", result)
+
+result = geometry.triangle(8,5)
+print("Area of triangle:", result)
+
+result = geometry.circle(10)
+print("Area of circle:", result)
